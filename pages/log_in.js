2c834572b76aa85f2ec6ea847d4ed3213064cd33
Daniel Longo <daniellongo@Daniels-MacBook.local>
2020-01-22 13:03:40 -0800

    submit code added

--- pages/log_in.js
+++ pages/log_in.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import {Image, View, ImageBackground, SafeAreaView, StyleSheet} from 'react-native';
+import {Image, View, ImageBackground, SafeAreaView, StyleSheet, ScrollView, StatusBar} from 'react-native';
 import { Container, Header, Content, Button, Text, Form, Item, Label, Input } from 'native-base';
+import PinView from 'react-native-pin-view' ;
 // import {ImageBackground} from "react-native-web";
 // import Image from "react-native-web/src/exports/Image";
 // let { StyleSheet } = React;
@@ -24,58 +25,115 @@ const styles = StyleSheet.create({
 
 
 class LogIn extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            showPinView : false
+        }
+    }
+
     render() {
+        console.log("TESTING", this.state.showPinView)
+        if (this.state.showPinView) {
+            console.log("showing pin view")
+            return (
+                <View
+                    style={{
+                        marginTop:"30%"
+                    }}
+                >
+                    <PinView
+                        onComplete={(val, clear)=>{alert(val)}}
+                        pinLength={5}
+                        showInputs={true}
+                        // inputBgColor={"#097eeb"}
+                        buttonTextColor={"#097eeb"}
+                        inputActiveBgColor={"#097eeb"}
+                    />
+                    <Button
+                        style={{marginLeft: "30%", textAlign:"center",  width:"42%", height:"9%"}}
+                        onPress={() => this.setState({showPinView : false})}
+                    >
+                        <Text style={{alignItems:"center", justifyContent:"center"}}>Cancel</Text>
+                    </Button>
+                </View>
+            )
+        }
+        else {
         return (
             <Container>
+                <StatusBar barStyle={'dark-content'}/>
         <ImageBackground source={require("../assets/bg-white.jpg")} style={{
             width: '110%',
             height: '110%',
             display: 'flex',
-            marginTop: '10%',
-            marginRight: '20%',
             marginBottom: '20%',
+            marginRight: '20%',
             flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
             resizeMode: 'cover',
         }}>
-            <Content
+            <ScrollView
             style={{
                 // display: 'flex',
-                marginTop: '30%',
+
+                paddingTop: '30%',
                 marginLeft :'20%',
                 width: '100%',
                 height: '100%',
+                // backgroundColor: 'green',
                 // flex : 1
             }}
             >
-                <Image
-                    source={require("../assets/logo-new.png")}
-                    style={{marginBottom:"20%"}}
-                />
-                <Button style={{marginBottom:"5%"}}><Text>Login</Text></Button>
-                <View
-                    style={{
-                        marginLeft:"10%"
-                    }}
-                >
-                    <Text style={{marginLeft:'40%'}}>OR</Text>
+                <View style={{width: '100%', height: '100%'}}>
+                    <Image
+                        source={require("../assets/logo-new.png")}
+                        style={{marginBottom:"15%"}}
+                    />
+                    <Input
+                        placeholder="Enter Email"
+                        style={{backgroundColor:"#f5f5f5", width:"100%", height:"70%", marginBottom:"3%"}}/>
+                    <Input
+                        placeholder="Enter Password"
+                        style={{backgroundColor:"#f5f5f5", width:"100%", height:"70%", marginBottom:"3%"}}
+                        secureTextEntry={true}/>
+                    <Button style={{marginLeft:"37%", marginBottom:"5%", width:"25%", height:"9%"}}><Text>Login</Text></Button>
+                    <View
+                        style={{
+                            marginLeft:"10%"
+                        }}
+                    >
+                        <Text style={{marginLeft:'40%'}}>OR</Text>
+                    </View>
+                    <Button
+                        style={{marginLeft: "30%", marginTop:"5%", textAlign:"center",  width:"42%", height:"9%", marginBottom:"0%"}}
+                        onPress={() => this.setState({showPinView : true})}
+                    >
+                        <Text style={{alignItems:"center", justifyContent:"center"}}>Submit Code</Text>
+                    </Button>
+                    <View style={{marginTop:"12%", display: 'flex', flexDirection: 'column'}}>
+                        <Text>
+                            Don't Have an Account or Code? <Text
+                            style={{color: 'blue'}}
+                            onPress={() => Linking.openURL('http://google.com')}>
+                            Sign Up
+                        </Text>
+                        </Text>
+                        <Text>
+                            Forgot Password? <Text
+                            style={{color: 'blue'}}
+                            onPress={() => Linking.openURL('http://google.com')}>
+                            Reset Password
+                        </Text>
+                        </Text>
+                    </View>
                 </View>
-                <Button style={{marginTop:"5%", textAlign:"center"}}><Text style={{alignItems:"center", justifyContent:"center"}}>Submit Code</Text></Button>
-                <View style={{marginTop:"1%"}}>
-                    <Text>
-                        Don't Have an Account or Code? <Text
-                        style={{color: 'blue'}}
-                        onPress={() => Linking.openURL('http://google.com')}>
-                        Sign Up
-                    </Text>
-                    </Text>
-                </View>
-            </Content>
+            </ScrollView>
             </ImageBackground>
         </Container>
         )
-    }
+    }}
 }
 
 export default LogIn
