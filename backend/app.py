1e644cecb919e7962c532ed0260cfc642e1dc8b3
Alex Krantz <alex@alexkrantz.com>
2020-01-23 13:51:36 -0800

    Add basic error handlers and loading from .env

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,23 @@
+from flask.cli import load_dotenv
+from flask import jsonify
 from os import getenv
 
 from database import db, create_app
 
-
 # Initialize app and database
+load_dotenv()
 app = create_app(__name__, getenv("DATABASE_URI"), True)
 db.create_all(app=app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.errorhandler(404)
+def not_found(_):
+    return jsonify({"status": "error", "reason": "not found"}), 404
+
+
+@app.errorhandler(405)
+def method_not_allowed(_):
+    return jsonify({"status": "error", "reason": "method not allowed"}), 405
 
 
 if __name__ == '__main__':
