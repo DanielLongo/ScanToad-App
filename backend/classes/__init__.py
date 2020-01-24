71fd1c79b906f8f60dc9d69a200915269ad6e84c
Alex Krantz <alex@alexkrantz.com>
2020-01-24 08:33:41 -0800

    Add describing specified class

--- backend/classes/__init__.py
+++ backend/classes/__init__.py
@@ -4,3 +4,4 @@ from .blueprint import classes
 # Register routes
 from .create import create
 from .list_method import list_method
+from .read import read
---
+++ backend/classes/read.py
@@ -0,0 +1,40 @@
+from flask import session
+
+from .blueprint import classes
+from database import Class, Student, Teacher
+from util import responses, login_required
+
+
+@classes.route("/<int:class_id>")
+@login_required
+def read(class_id):
+    # Ensure class exists
+    c = Class.query.filter_by(id=class_id).first()
+    if c is None:
+        return responses.error("specified class does not exist", 404)
+
+    # Retrieve user from database
+    if session["type"] == "teacher":
+        user = Teacher.query.filter_by(id=session["uid"]).first()
+    elif session["type"] == "student":
+        user = Student.query.filter_by(id=session["uid"]).first()
+    else:
+        return responses.error("invalid account type", 500)
+
+    # Ensure user still exists
+    if user is None:
+        session.clear()
+        return responses.error("logged in user no longer exists", 403)
+
+    # Ensure user part of/owns class
+    if session["type"] == "teacher" and c.teacher_id != session["uid"]:
+        return responses.error("user does not own class", 403)
+    elif session["type"] == "student":
+        student = Student.query.filter_by(id=session["uid"]).first()
+        if student is None:
+            session.clear()
+            return responses.error("logged in user no longer exists", 403)
+        elif student.class_id != c.id:
+            return responses.error("user not part of specified class", 403)
+
+    return responses.success_with_data(c.to_json())
