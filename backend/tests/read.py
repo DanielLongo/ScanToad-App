854a90db6f0bedae64693feb06f980a4d84c004b
Alex Krantz <alex@alexkrantz.com>
2020-01-24 16:38:14 -0800

    Fix permission validation

--- backend/tests/read.py
+++ backend/tests/read.py
@@ -1,7 +1,7 @@
 from flask import session
 
 from .blueprint import tests
-from database import Class, Test
+from database import Class, Student, Test
 from util import responses, login_required
 
 
@@ -13,9 +13,15 @@ def read(class_id, test_id):
     if c is None:
         return responses.error("specified class does not exist", 404)
 
-    # Ensure teacher owns class
-    if c.teacher_id != session["uid"]:
+    # Ensure teacher owns class or student in class
+    if session["type"] == "teacher" and c.teacher_id != session["uid"]:
         return responses.error("user does not own class", 403)
+    elif session["type"] == "student":
+        s = Student.query.filter_by(id=session["uid"]).first()
+        if s is None:
+            return responses.error("authenticated user no longer exists", 400)
+        elif s.class_id != class_id:
+            return responses.error("user not in class", 403)
 
     # Ensure test exists
     test = Test.query.filter_by(id=test_id).first()
